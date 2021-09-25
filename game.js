ef8d978c6c1668116f8e58a6e0e1bc82230ed964
Clayton Grassick <clayton@claytronics.org>
2021-09-25 15:33:29 -0400

    Working game

--- game.js
+++ game.js
@@ -15,6 +15,14 @@ loadSprite("laser", "laser.png", {
   }
 })
 
+loadSprite("explosion", "exp2_0.png", {
+  sliceX: 4,
+  sliceY: 4,
+  anims: {
+    boom: {from: 0, to: 15}
+  }
+})
+loadSound("explosion", "explosion.wav")
 
 // define a scene
 scene("main", () => {
@@ -86,220 +94,53 @@ scene("main", () => {
     destroy(meteor)
     destroy(laser)
 
-   //  play("explosion")
+    const explosion = add([
+      scale(3),
+      sprite("explosion"),
+      pos(meteor.pos),
+      origin("center")
+    ])
+    explosion.play("boom")
 
-    // const explosion = add([
-    //   sprite("explosion"),
-    //   pos(missile.pos),
-    //   origin("center")
-    // ])
-    // explosion.play("boom")
-
-    // wait(1, () => {
-    //   destroy(explosion)
-    // });
-
-    // const explosion2 = add([
-    //   sprite("explosion"),
-    //   pos(tank.pos),
-    //   origin("center"),
-    //   scale(5)
-    // ])
-    // explosion2.play("boom")
-
-    // wait(1, () => {
-    //   destroy(explosion2)
-    //   go("start")
-    // });
+    wait(1, () => {
+      destroy(explosion)
+    });
+    play("explosion")
   })
 
   collides("sr", "meteor", (sr, meteor) => {
     destroy(meteor)
     destroy(sr)
 
-   //  play("explosion")
+    const explosion = add([
+      scale(4),
+      sprite("explosion"),
+      pos(sr.pos),
+      origin("center")
+    ])
+    explosion.play("boom")
 
-    // const explosion = add([
-    //   sprite("explosion"),
-    //   pos(missile.pos),
-    //   origin("center")
-    // ])
-    // explosion.play("boom")
-
-    // wait(1, () => {
-    //   destroy(explosion)
-    // });
-
-    // const explosion2 = add([
-    //   sprite("explosion"),
-    //   pos(tank.pos),
-    //   origin("center"),
-    //   scale(5)
-    // ])
-    // explosion2.play("boom")
-
-    // wait(1, () => {
-    //   destroy(explosion2)
-    //   go("start")
-    // });
+    wait(1, () => {
+      destroy(explosion)
+      go("end")
+    });
+    play("explosion")
   })
-
-
-  // keyDown("4", () => {
-  //   blue.angle += dt() * 3
-  // })
-
-  // keyDown("6", () => {
-  //   blue.angle -= dt() * 3
-  // })
-
-  // keyDown("5", () => {
-  //   blue.move(100 * Math.cos(blue.angle), -100 * Math.sin(blue.angle))
-  // })
-
-  // keyDown("2", () => {
-  //   blue.move(-100 * Math.cos(blue.angle), +100 * Math.sin(blue.angle))
-  // })
-
-  // keyPress("8", () => {
-  //   if (blue.exists()) {
-  //     add([
-  //       sprite("missile"),
-  //       pos(blue.pos),
-  //       scale(0.1),
-  //       rotate(blue.angle),
-  //       origin("center"),
-  //       color(0.5, 0.5, 1),
-  //       "missile",
-  //       "missileblue"
-  //     ])
-  //     play("launch", { volume: 0.1 })
-  //   }
-  // })
-
-  // action("missile", (obj) => {
-  //   obj.move(200 * Math.cos(obj.angle), -200 * Math.sin(obj.angle));
-  // });
-
-  // keyPress("w", () => {
-  //   if (red.exists()) {
-  //     add([
-  //       sprite("missile"),
-  //       pos(red.pos),
-  //       scale(0.1),
-  //       rotate(red.angle),
-  //       origin("center"),
-  //       color(1, 0.3, 0.3),
-  //       "missile",
-  //       "missilered",
-  //     ])
-  //     play("launch", { volume: 0.1 })
-  //   }
-  // })
-
-  // collides("missilered", "tankblue", (missile, tank) => {
-  //   destroy(missile)
-  //   destroy(tank)
-
-  //   play("explosion")
-
-  //   const explosion = add([
-  //     sprite("explosion"),
-  //     pos(missile.pos),
-  //     origin("center")
-  //   ])
-  //   explosion.play("boom")
-
-  //   wait(1, () => {
-  //     destroy(explosion)
-  //   });
-
-  //   const explosion2 = add([
-  //     sprite("explosion"),
-  //     pos(tank.pos),
-  //     origin("center"),
-  //     scale(5)
-  //   ])
-  //   explosion2.play("boom")
-
-  //   wait(1, () => {
-  //     destroy(explosion2)
-  //     go("start")
-  //   });
-
-  // });
-
-  // collides("missileblue", "tankred", (missile, tank) => {
-  //   destroy(missile)
-  //   destroy(tank)
-
-  //   play("explosion")
-
-  //   const explosion = add([
-  //     sprite("explosion"),
-  //     pos(missile.pos),
-  //     origin("center")
-  //   ])
-  //   explosion.play("boom")
-
-  //   wait(1, () => {
-  //     destroy(explosion)
-  //   });
-
-  //   const explosion2 = add([
-  //     sprite("explosion"),
-  //     pos(tank.pos),
-  //     origin("center"),
-  //     scale(5)
-  //   ])
-  //   explosion2.play("boom")
-
-  //   wait(1, () => {
-  //     destroy(explosion2)
-  //     go("start")
-  //   });
-  // });
-
-  // collides("missile", "wall", (missile, wall) => {
-  //   destroy(missile)
-  //   play("explosion", { 
-  //     volume: 0.1
-  //   })
-
-  //   const explosion = add([
-  //     sprite("explosion"),
-  //     pos(missile.pos),
-  //     origin("center")
-  //   ])
-  //   explosion.play("boom")
-
-  //   wait(1, () => {
-  //     destroy(explosion)
-  //   });
-
-  // });
-
-  // collides("missile", "missile", (missile1, missile2) => {
-  //   return
-
-  //   // remove both the bullet and the thing bullet hit with tag "killable" from scene
-  //   destroy(missile1)
-  //   destroy(missile2)
-  //   const explosion = add([
-  //     sprite("explosion"),
-  //     pos(missile1.pos),
-  //     origin("center")
-  //   ])
-  //   explosion.play("boom")
-
-  //   wait(1, () => {
-  //     destroy(explosion)
-  //   });
-
-  // });
-
 })
 
+
+scene("end", () => {
+  add([
+    pos(width()/2, 100),
+    origin("center"),
+    color(0, 0, 1),
+    text(`game over!!!`, 40),
+  ])
+
+  keyPress("enter", () => {
+    go("main")
+  })
+})
 
 // start the game
 go("main");
